d9a5f137fd282e97d454161bc4efaa02f2a5b0cf
Zanganss <76145765+Zanganss@users.noreply.github.com>
2021-02-07 20:13:25 +0800

    Update nzwa.js

--- nzwa.js
+++ nzwa.js
@@ -925,17 +925,81 @@ async function starts() {
 					break*/
 				case 'nulis':
 				case 'tulis':
-					if (args.length < 1) return reply('Yang mau di tulis apaan?')
-                                        if (!isRegister) return reply(mess.only.daftarB)
-                                        if (isLimit(sender)) return reply(ind.limitend(pusname))
-					teks = body.slice(7)
-					reply(mess.wait)
-					anu = await fetchJson(`https://mhankbarbar.tech/nulis?text=${teks}&apiKey=${BarBarKey}`, {method: 'get'})
-					if (anu.error) return reply(anu.error)
-					buff = await getBuffer(anu.result)
-					client.sendMessage(from, buff, image, {quoted: mek, caption: mess.success})
-                                        await limitAdd(sender)
-					break
+					let util = require('util')
+let path = require('path')
+let { spawn } = require('child_process')
+
+// Font By MFarelS:V
+let fontPath = 'src/font/Zahraaa.ttf'
+let handler  = async (m, { conn, args }) => {
+  let inputPath ='assets/nulis.jpg'
+  let outputPath = 'tmp/hasil.jpg'
+  let d = new Date
+  let tgl = d.toLocaleDateString('id-Id')
+  let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
+  let teks = args.join` `
+  // conn.reply(m.chat, util.format({fontPath, inputPath, outputPath, tgl, hari, teks}), m)
+  spawn('convert', [
+    inputPath,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '20',
+    '-interline-spacing',
+    '1',
+    '-annotate',
+    '+806+78',
+    hari,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '18',
+    '-interline-spacing',
+    '1',
+    '-annotate',
+    '+806+102',
+    tgl,
+    '-font',
+    fontPath,
+    '-size',
+    '1024x784',
+    '-pointsize',
+    '20',
+    '-interline-spacing',
+    '-7.5',
+    '-annotate',
+    '+344+142',
+    teks,
+    outputPath
+  ])
+  .on('error', e => conn.reply(m.chat, util.format(e), m))
+  .on('exit', () => {
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Hati² ketahuan:v', m)
+  })
+}
+handler.help = ['n'].map(v => v + 'ulis <teks>')
+handler.tags = ['tools']
+handler.command = /^nulis$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+// BY MFARELS NJEENK
+// https://GitHub.com/MFarelS/
+					
 	case 'kbbi':
             client.updatePresence(from, Presence.composing) 
                 if (args.length < 1) return reply(`Masukan Pertanyaan\nContoh : ${prefix}kbbi asu`)
